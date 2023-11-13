3caf25abdd6d16f59affa904e710bb2efa458d4a
Raju271127 <133193027+Raju271127@users.noreply.github.com>
2023-11-13 08:51:32 +0530

    Update authActions.js

--- frontend/src/actions/authActions.js
+++ frontend/src/actions/authActions.js
@@ -25,20 +25,19 @@ export const loginUser = userData => dispatch => {
     axios
     .post("https://job-search-applicatoin-api.onrender.com/user/login", userData)
     .then(res => 
-    //     {
-    //     // Save to localStorage
-    //     // Set token to localStorage
-    //     const { token } = res.data;
-    //     localStorage.setItem("jwtToken", token);
-    //     // Set token to Auth header
-    //     setAuthToken(token);
-    //     // Decode token to get user data
-    //     const decoded = jwt_decode(token);
-    //     // Set current user
-    //     dispatch(setCurrentUser(decoded));
-    // }
-    
-        alert("User registered successfully! You can log in now. "),history.push("/dashboard")) 
+        {
+        // Save to localStorage
+        // Set token to localStorage
+        const { token } = res.data;
+        localStorage.setItem("jwtToken", token);
+        // Set token to Auth header
+        setAuthToken(token);
+        // Decode token to get user data
+        const decoded = jwt_decode(token);
+        // Set current user
+        dispatch(setCurrentUser(decoded));
+    }
+) 
          
     .catch(err =>
             dispatch({
